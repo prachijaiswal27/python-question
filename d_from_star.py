137c46fea2aa522725900896fac596d33c562466
prachijaiswal27 <prachijaiswal015@gmail.com>
2026-04-25 23:22:27 -0700

    Add files via upload

---
+++ d_from_star.py
@@ -0,0 +1,9 @@
+for row in range(7):
+    for col in range(5):
+        if (col == 0) or \
+           (row == 0 or row == 6) and col < 4 or \
+           (col == 4 and row != 0 and row != 6):
+            print("*", end=" ")
+        else:
+            print(" ", end=" ")
+    print()
